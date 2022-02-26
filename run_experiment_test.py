d016fe536902cb213f5f21f2537829bbf4e0e7ae
Neo Zhou <zhouaea@bc.edu>
2022-02-26 16:41:02 -0500

    fix typo in experiment test

--- run_experiment_test.py
+++ run_experiment_test.py
@@ -4,6 +4,8 @@ import concurrent.futures
 from setup_nodes import setup_nodes
 from run_experiment import run_experiment
 
+from setup_network_delay import setup_delays
+
 if len(sys.argv) != 2:
     sys.stderr.write('Usage: python3 %s <config_file>\n' % sys.argv[0])
     sys.exit(1)
@@ -13,6 +15,7 @@ config = json.load(config_file)
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
     timestamp = setup_nodes(config, executor)
-    run_experiment(config, timestamp, executor)
+    server_names_to_internal_ips = setup_delays(config, executor)
+    run_experiment(server_names_to_internal_ips, config, timestamp, executor)
 
 config_file.close()
