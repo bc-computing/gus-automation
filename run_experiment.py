62f65bc9fb6afb92079958ae6b2163036a8259a9
Neo Zhou <zhouaea@bc.edu>
2022-02-27 16:36:45 -0500

    sleep longer so servers can initialize properly

--- run_experiment.py
+++ run_experiment.py
@@ -87,7 +87,7 @@ def start_servers(config, timestamp, server_names_to_internal_ips):
         servers_started += 1
 
     # I assume there is no way we can detect when the servers are initialized.
-    time.sleep(2)
+    time.sleep(5)
     return server_threads
 
 def start_clients(config, timestamp, server_names_to_internal_ips):
