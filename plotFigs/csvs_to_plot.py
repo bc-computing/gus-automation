6f9614a52c98a77dd7d060445c8a0ff18e8df47b
Neo Zhou <zhouaea@bc.edu>
2022-03-10 23:48:18 -0500

    refactor plotting functions

--- plotFigs/csvs_to_plot.py
+++ plotFigs/csvs_to_plot.py
@@ -53,41 +53,6 @@ def generate_cdf_gnuplot_script(plot_script_file, plot_target_directory, csvs, p
             if i != len(csvs) - 1:
                 f.write(', \\\n')
 
-# for figure 8
-def write_ratio_throughput_csvs_to_plot(plot_target_directory, gryff_csv, gus_csv, epaxos_csv):
-    plot_script_file = os.path.join(plot_target_directory, 'write_ratio-throughput.gpi')
-
-    csvs = (gryff_csv, gus_csv, epaxos_csv)
-    protocols = ["Gryff", "Gus", "EPaxos"]  # should match with csvs passed in
-    generate_write_ratio_throughput_gnuplot_script(plot_script_file, plot_target_directory, csvs, protocols)
-    subprocess.call(['gnuplot', plot_script_file])
-
-def generate_write_ratio_throughput_gnuplot_script(plot_script_file, plot_target_directory, csvs, protocols):
-    with open(plot_script_file, 'w+') as f:
-        # f.write("set datafile separator ','\n")
-        f.write("set terminal pngcairo size 575,250 enhanced font 'Helvetica,18'\n")
-
-        f.write("set key center tmargin\n")
-        f.write("set key horizontal\n")
-
-        f.write("set xlabel 'Fraction of Writes'\n")
-        f.write("set ylabel 'Throughput (ops/s)'\n")
-
-        f.write("set xtics .2\n")
-        f.write("set ytics 1000\n")
-
-        f.write('set output \'%s/%s\'\n' % (plot_target_directory, os.path.splitext(os.path.basename(plot_script_file))[0] + '.png'))
-
-        f.write('set style line 1 linecolor "web-green" linewidth 6 pointtype 26 pointsize 1 dashtype 4\n')
-        f.write('set style line 2 linecolor "orange" linewidth 6 pointtype 20 pointsize 1 dashtype 1\n')
-        f.write('set style line 3 linecolor "blue" linewidth 6 pointtype 22 pointsize 1 dashtype 3\n')
-
-        f.write('plot ')
-        for i in range(len(csvs)):
-            f.write("'%s' title '%s' with linespoints linestyle %d" % (csvs[i], protocols[i], i + 1))
-            if i != len(csvs) - 1:
-                f.write(', \\\n')
-
 # for figure 9
 def write_ratio_throughput_csvs_to_plot(plot_target_directory, gryff_csv, gus_csv, epaxos_csv):
     plot_script_file = os.path.join(plot_target_directory, 'write_ratio-throughput.gpi')
