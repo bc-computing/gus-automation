8cd44e2fb228600d51cd924bea586fdc26e78ad5
cadumas01 <cadumas01@gmail.com>
2022-07-07 10:13:54 -0500

    update setconfig

--- set_config.py
+++ set_config.py
@@ -3,12 +3,13 @@ import os
 
 def get_user():
     user = os.system("whoami")
-    print("whoami= " + user)
+    print("whoami= " , user)
 
 def main():
-    update_json.update("configs/fig6.json","cloud_user",get_user() )
+    update_json.update("configs/fig6.json","cloud_user", get_user() )
 
 if __name__ == "__main__":
     print("Setting config...")
     main()
 
+# Maybe pipe whoami in as an arugment??? . whoami = 0 is what is currently happening when python runs it
