837cf6ee482081661c1b25ddc24c957c027df2cb
cadumas01 <cadumas01@gmail.com>
2022-07-25 12:05:48 -0400

    check cmd updated

--- utils/command_util.py
+++ utils/command_util.py
@@ -5,10 +5,11 @@ from utils.remote_util import *
 # runs a unix command and returns the output (that would be printed to stdout)
 def check_cmd_output(cmd):
    # output = subprocess.check_output(cmd)
-    #return output.decode("utf-8").strip("\n") 
-
+   
     ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-    return ps.communicate()[0]
+    output = ps.communicate()[0]
+    return output.decode("utf-8").strip("\n") 
+
 
 
 
