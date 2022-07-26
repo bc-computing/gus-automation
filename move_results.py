a1928d79ba8952e7d4ffed3792990b5285131d2f
root <root@control.test.hyflowtm-pg0.utah.cloudlab.us>
2022-07-26 13:37:19 -0600

    move results 2 can be controlled remmotely?

--- move_results.py
+++ move_results.py
@@ -29,26 +29,32 @@ def move_results(config_file_path):
 
 def move_results2(config_file_path):
     # Since results are by date, this gets most recent results
-    results = check_cmd_output("ls results | sort -r | head -n 1")
+    results = check_cmd_output("ls /root/go/src/gus-automation/results | sort -r | head -n 1")
+
     print(results)
 
     config_file = open(config_file_path)
 
     config = json.load(config_file)
 
-    user = sys.stdin.readlines()
-    print("user = ")
+    user = sys.stdin.readline().strip()
+    print("user = " + user)
 
     # need to create results first
     destination_parent_path = "/users/" + user + "/results/"
     destination_path = destination_parent_path + results
 
     source_path = config["base_control_experiment_directory"] + "/" + results
+    print("source_path = " + source_path) 
     os.system("mkdir -p " + destination_parent_path) 
+    print("just made dir, destination_parent_path=" + destination_parent_path)
 
+    print("destination_path = " + destination_path)
+    print(os.path.exists(destination_path))
    # If the experiment results haven't been copied already, copy them
     if not os.path.exists(destination_path):
-            os.system("cp -r " + source_path + " " + destination_path)
+        print("About to copy...")
+        os.system("cp -r " + source_path + " " + destination_path)
 
 
 def usage():
